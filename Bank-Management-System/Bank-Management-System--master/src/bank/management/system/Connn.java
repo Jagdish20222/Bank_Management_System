fdb07f31e5a79292961bb08ff2c63d827b58fe3a
Jagdish_Aade <162777721+Jagdish20222@users.noreply.github.com>
2025-01-25 10:47:26 +0530

    Update Connn.java

--- Bank-Management-System/Bank-Management-System--master/src/bank/management/system/Connn.java
+++ Bank-Management-System/Bank-Management-System--master/src/bank/management/system/Connn.java
@@ -7,7 +7,7 @@ public class Connn {
     Statement statement;
     public Connn(){
         try{
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","J@gDISH6174");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","mysqlpaas");
             statement = connection.createStatement();
         }catch (Exception e){
             e.printStackTrace();
